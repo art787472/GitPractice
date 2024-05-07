db4cd4b8a318ca2b1863972be0248f9634abdb12
周政輝 <leo604leo604@gmail.com>
2024-05-07 12:02:25 +0800

    完成Models與Service建立，並新增User Model

---
+++ git練習/Models/UserModel.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace git練習.Models
+{
+    public class UserModel
+    {
+        public String UserName { get; set; }
+        public String Account { get; set; }
+        public String Password { get; set; }
+        public int Age { get; set; }
+
+        public UserModel(string userName, string account, string password, int age)
+        {
+            UserName = userName;
+            Account = account;
+            Password = password;
+            Age = age;
+        }
+    }
+}
