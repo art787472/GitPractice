ff7336352e4617c0abb3ee7e93de7146c1a5086f
Chen Yi-Cheng <art787472@gmail.com>
2024-05-07 12:37:48 +0800

    User新增password欄位

--- git練習/Model/User.cs
+++ git練習/Model/User.cs
@@ -8,5 +8,9 @@ namespace git練習
 {
     internal class User
     {
+        public string id {  get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
     }
 }
