ccac00ed13bb9435ee7c1169ec67a0ec76cea6e6
周政輝 <leo604leo604@gmail.com>
2024-05-09 13:08:51 +0800

    重構程式

--- git練習/Program.cs
+++ git練習/Program.cs
@@ -11,25 +11,12 @@ namespace git練習
     {
         static void Main(string[] args)
         {
-            string account = Console.ReadLine();
-            string password = Console.ReadLine();
-            string userName = Console.ReadLine();
             RegisterService service = new RegisterService();
-            bool isSuccess = service.Register(account, password, userName);
-            if (isSuccess)
+            bool isSuccess = service.Register(Console.ReadLine(), Console.ReadLine(), Console.ReadLine());
+            if (!isSuccess)
             {
                 Console.Write("登入成功!!");
             }
-            else
-            {
-                Console.WriteLine("登入失敗");
-            }
-            LoginFunction loginFunction = new LoginFunction();
-            var (isLoginSuccess, loginMessage) = loginFunction.Login("leo604leo604@gmail.com", "abc1234", "Leo");
-            Console.WriteLine(loginMessage);
-
-            loginFunction.Logout();
-
             Console.ReadKey();
         }
     }
